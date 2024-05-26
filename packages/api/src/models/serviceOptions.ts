d111b22befd598a54e4cc860663251264e52ba16
Mike Seese <seesemichaelj@gmail.com>
2024-05-25 19:51:47 -0700

    revert 1.0.7 axios change

--- packages/api/src/models/serviceOptions.ts
+++ packages/api/src/models/serviceOptions.ts
@@ -1,10 +1,7 @@
 /** Generate by swagger-axios-codegen */
 /* eslint-disable */
 // @ts-nocheck
-import { AxiosInstance, AxiosRequestConfig } from 'axios';
-
-// require like this to enable vercel/pkg to package it
-const axiosStatic = require("axios/dist/node/axios.cjs");
+import axiosStatic, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 export interface IRequestOptions extends AxiosRequestConfig {
   /** only in axios interceptor config*/
